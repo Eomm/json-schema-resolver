db5f94415ad2a77c22336b95093b74873671a0be
Manuel Spigolon <behemoth89@gmail.com>
2020-05-15 20:37:54 +0200

    $ref replacing

--- ref-resolver.js
+++ ref-resolver.js
@@ -1,66 +1,101 @@
 'use strict'
 
+const debug = require('debug')('json-schema-resolver')
 const URI = require('uri-js')
 const { EventEmitter } = require('events')
 
-// Target: DRAFT-07
+const kRefToDef = Symbol('json-schema-resolver.refToDef')
+const kConsumed = Symbol('json-schema-resolver.consumed')
+const kIgnore = Symbol('json-schema-resolver.ignore')
+
+// ! Target: DRAFT-07
 // https://tools.ietf.org/html/draft-handrews-json-schema-01
 
-// Open to DRAFT 08
+// ? Open to DRAFT 08
 // https://json-schema.org/draft/2019-09/json-schema-core.html
 
 // TODO logic: https://json-schema.org/draft/2019-09/json-schema-core.html#rfc.appendix.B.1
-function jschemaResolver (rootSchema, options) {
-  const opts = options || {}
-  const { externalSchemas } = opts
-
-  // If present, the value for this keyword MUST be a string, and MUST
-  // represent a valid URI-reference [RFC3986].  This value SHOULD be
-  // normalized, and SHOULD NOT be an empty fragment <#> or an empty
-  // string <>.
-  const idExploded = URI.parse(rootSchema.$id)
-  idExploded.fragment = undefined // remove fragment
-
-  const baseUri = URI.serialize(idExploded) // canonical absolute-URI
-  rootSchema.$id = baseUri // fix the schema $id value
+function jsonSchemaResolver () {
+  const ee = new EventEmitter()
 
   const allIds = new Map()
+  let rolling = 0
+  ee.on('$id', collectIds)
+
   const allRefs = []
-
-  if (externalSchemas) {
-    externalSchemas.forEach(_ => {
-      const ids = mapIds(idExploded, _)
-      ids.on('$id', collectIds)
-      ids.on('$ref', collectRefs)
-    })
-  }
-  const ids = mapIds(idExploded, rootSchema)
-  ids.on('$id', collectIds)
-  ids.on('$ref', collectRefs)
-
-  // TODO attach every external schema to rootSchema/definition (or $defs for draft 08)
-  // TODO rewrite all the $ref pointing to /definitions
+  ee.on('$ref', collectRefs)
 
   return {
+    resolve
+  }
 
+  function resolve (rootSchema, opts) {
+    const { externalSchemas } = opts || {}
+
+    // If present, the value for this keyword MUST be a string, and MUST
+    // represent a valid URI-reference [RFC3986].  This value SHOULD be
+    // normalized, and SHOULD NOT be an empty fragment <#> or an empty
+    // string <>.
+    const appUri = URI.parse(rootSchema.$id)
+    appUri.fragment = undefined // remove fragment
+    debug('Found app URI %o', appUri)
+
+    if (externalSchemas) {
+      for (const es of externalSchemas) { mapIds(ee, appUri, es) }
+    }
+    debug('Processed external schemas')
+
+    const baseUri = URI.serialize(appUri) // canonical absolute-URI
+    rootSchema.$id = baseUri // fix the schema $id value
+    rootSchema[kIgnore] = true
+
+    mapIds(ee, appUri, rootSchema)
+    debug('Processed root schema')
+
+    debug('Generating %d refs', allRefs.length)
+    allRefs.forEach(({ baseUri, ref, json }) => {
+      debug('Evaluating $ref %s', ref)
+      if (ref[0] === '#') { return }
+
+      const evaluatedJson = allIds.get(baseUri)
+      evaluatedJson[kConsumed] = true
+      json.$ref = `#/definitions/${evaluatedJson[kRefToDef]}`
+    })
+
+    // TODO $def instead of definitions
+    allIds.forEach((json, baseUri) => {
+      if (json[kConsumed] === true) {
+        if (!rootSchema.definitions) {
+          rootSchema.definitions = {}
+        }
+
+        rootSchema.definitions[json[kRefToDef]] = json
+      }
+    })
+
+    return rootSchema
   }
 
   function collectIds (json, baseUri, relative) {
+    if (json[kIgnore]) { return }
+
     const rel = (relative && URI.serialize(relative)) || ''
     const id = URI.serialize(baseUri) + rel
-    console.log(id)
     if (allIds.has(id)) {
-      console.log('WARN duplicated id .. IGNORED - ' + id)
+      debug('WARN duplicated id %s .. IGNORED - ', id) // TODO
     } else {
+      debug('Collected $id %s', id)
+      json[kRefToDef] = `def-${rolling++}`
       allIds.set(id, json)
     }
   }
 
   function collectRefs (json, baseUri, refVal) {
     const refUri = URI.parse(refVal)
-    console.log('Ref: ' + URI.serialize(refUri))
+    debug('Pre enqueue $ref %o', refUri)
+    if (refUri.reference === 'same-document') {
 
-    if (refUri.reference !== 'absolute') {
+    } else if (refUri.reference !== 'absolute') {
       refUri.scheme = baseUri.scheme
       refUri.userinfo = baseUri.userinfo
       refUri.host = baseUri.host
@@ -73,55 +108,51 @@ function jschemaResolver (rootSchema, options) {
       }
     }
 
+    const ref = URI.serialize(refUri)
     allRefs.push({
       baseUri: URI.serialize(baseUri),
-      ref: URI.serialize(refUri),
+      ref,
       json
     })
+    debug('Enqueue $ref %s', ref)
   }
 }
 
-function mapIds (baseUri, schema) {
-  const ee = new EventEmitter()
-  process.nextTick(() => { search(baseUri, schema) })
-  return ee
-
-  /**
+/**
    *
    * @param {URI} baseUri
    * @param {*} json
    */
-  function search (baseUri, json) {
-    if (!(json instanceof Object)) return
+function mapIds (ee, baseUri, json) {
+  if (!(json instanceof Object)) return
 
-    if (json.$id) { // TODO the $id should manage $anchor to support draft 08
-      const $idUri = URI.parse(json.$id)
-      let fragment = null
+  if (json.$id) { // TODO the $id should manage $anchor to support draft 08
+    const $idUri = URI.parse(json.$id)
+    let fragment = null
 
-      if ($idUri.reference === 'absolute') {
-        // "$id": "http://example.com/root.json"
-        baseUri = $idUri // a new baseURI for children
-      } else if ($idUri.reference === 'relative') {
-        // "$id": "other.json",
-        const newBaseUri = Object.assign({}, baseUri)
-        newBaseUri.path = $idUri.path
-        newBaseUri.fragment = $idUri.fragment
-        baseUri = newBaseUri
-      } else {
-        // { "$id": "#bar" }
-        fragment = $idUri
-      }
-      ee.emit('$id', json, baseUri, fragment)
+    if ($idUri.reference === 'absolute') {
+      // "$id": "http://example.com/root.json"
+      baseUri = $idUri // a new baseURI for children
+    } else if ($idUri.reference === 'relative') {
+      // "$id": "other.json",
+      const newBaseUri = Object.assign({}, baseUri)
+      newBaseUri.path = $idUri.path
+      newBaseUri.fragment = $idUri.fragment
+      baseUri = newBaseUri
+    } else {
+      // { "$id": "#bar" }
+      fragment = $idUri
     }
+    ee.emit('$id', json, baseUri, fragment)
+  }
 
-    const fields = Object.keys(json)
-    for (const prop of fields) {
-      if (prop === '$ref') {
-        ee.emit('$ref', json, baseUri, json[prop])
-      }
-      search(baseUri, json[prop])
+  const fields = Object.keys(json)
+  for (const prop of fields) {
+    if (prop === '$ref') {
+      ee.emit('$ref', json, baseUri, json[prop])
     }
+    mapIds(ee, baseUri, json[prop])
   }
 }
 
-module.exports = jschemaResolver
+module.exports = jsonSchemaResolver
--- test/ref-resolver.test.js
+++ test/ref-resolver.test.js
@@ -2,25 +2,29 @@
 
 const { test } = require('tap')
 const refResolver = require('../ref-resolver')
+const clone = require('rfdc')({ circles: false })
 
 // https://json-schema.org/draft/2019-09/json-schema-core.html#idExamples
 
-test('', t => {
+test('$ref to root', t => {
+  t.plan(2)
+
   const schema = {
     $id: 'http://example.com/root.json',
+    properties: {
+      person: { $ref: '#/definitions/person' },
+      children: {
+        type: 'array',
+        items: { $ref: '#/definitions/person' }
+      }
+    },
     definitions: {
-      A: { $id: '#foo' },
-      B: {
-        $id: 'other.json',
-        definitions: {
-          X: { $id: '#bar' },
-          Z: { $ref: 'commonSchema#/definitions/hello' },
-          Zx: { $ref: 'http://wow/commonSchema#/definitions/hello' },
-          Y: { $id: 't/inner.json' }
+      person: {
+        type: 'object',
+        properties: {
+          name: { type: 'string' },
+          age: { type: 'integer' }
         }
-      },
-      C: {
-        $id: 'urn:uuid:ee564b8a-7a87-4125-8c96-e9f123d6766f'
       }
     }
   }
@@ -36,6 +40,91 @@ test('', t => {
       }
     ]
   }
-  refResolver(schema, opts)
-  t.end()
+  const resolver = refResolver()
+  const originalSchema = clone(schema)
+  const out = resolver.resolve(schema, opts)
+  t.deepEquals(schema, originalSchema, 'the param schema should not be changed')
+  t.equals(schema, out, 'the output schema is the same (LINK) of the input one')
+})
+
+test('$ref to an external schema', t => {
+  t.plan(3)
+  const schema = {
+    $id: 'http://example.com/SimplePerson',
+    type: 'object',
+    properties: {
+      name: { type: 'string' },
+      address: { $ref: '/SimpleAddress' },
+      votes: { type: 'integer', minimum: 1 }
+    }
+  }
+
+  const opts = {
+    externalSchemas: [
+      {
+        $id: '/SimpleAddress',
+        type: 'object',
+        properties: {
+          lines: {
+            type: 'array',
+            items: { type: 'string' }
+          },
+          zip: { type: 'string' },
+          city: { type: 'string' },
+          country: { type: 'string' }
+        },
+        required: ['country']
+      }
+    ]
+  }
+
+  const resolver = refResolver()
+  const out = resolver.resolve(schema, opts)
+  t.deepEquals(schema, out, 'the output is the same input modified')
+  t.ok(out.definitions, 'definitions has been added')
+  t.deepEquals(Object.values(out.definitions), opts.externalSchemas, 'external schema has been added to definitions')
+})
+
+test('$ref circular', t => {
+  t.plan(3)
+  const schema = {
+    $id: 'http://example.com/',
+    $ref: 'SimplePerson'
+  }
+
+  const opts = {
+    externalSchemas: [
+      {
+        $id: 'SimplePerson',
+        type: 'object',
+        properties: {
+          name: { type: 'string' },
+          address: { $ref: 'SimpleAddress#' },
+          friends: { type: 'array', items: { $ref: '#' } },
+          votes: { type: 'integer', minimum: 1 }
+        }
+      },
+      {
+        $id: 'SimpleAddress',
+        type: 'object',
+        properties: {
+          lines: {
+            type: 'array',
+            items: { type: 'string' }
+          },
+          zip: { type: 'string' },
+          city: { type: 'string' },
+          country: { type: 'string' }
+        },
+        required: ['country']
+      }
+    ]
+  }
+
+  const resolver = refResolver()
+  const out = resolver.resolve(schema, opts)
+  t.deepEquals(schema, out, 'the output is the same input modified')
+  t.ok(out.definitions, 'definitions has been added')
+  require('fs').writeFileSync('./out.json', JSON.stringify(out, null, 2))
+  t.deepEquals(Object.values(out.definitions), opts.externalSchemas, 'external schema has been added to definitions')
 })
