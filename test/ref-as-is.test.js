2b0c91d106a02bd287b74dc584ce9ff227d17107
Manuel Spigolon <manuel.spigolon@nearform.com>
2021-08-30 15:58:11 +0200

    more tests

--- test/ref-as-is.test.js
+++ test/ref-as-is.test.js
@@ -1,5 +1,6 @@
 
 const { test } = require('tap')
+const URI = require('uri-js')
 const RefResolver = require('../ref-resolver')
 
 test('resolving schema within the $id', t => {
@@ -70,4 +71,46 @@ test('resolving schema within the $id case #2', t => {
   const externalDef = resolver.definitions()
   t.ok(externalDef.definitions['urn%3Aschema%3Abase'], 'buildLocalReference result')
   t.ok(externalDef.definitions['urn%3Aschema%3Aref'], 'buildLocalReference result')
+})
+
+test('resolving schema within the $id relative', t => {
+  t.plan(4)
+
+  const schema = {
+    type: 'object',
+    properties: {
+      user: { $ref: 'http://hello.absolute/user.json#/definitions/name' },
+      address: { $ref: 'adr#/definitions/street' }
+    }
+  }
+
+  const opts = {
+    applicationUri: 'http://hello.relative',
+    externalSchemas: [
+      {
+        $id: 'http://hello.absolute/user.json',
+        type: 'object',
+        definitions: { name: { type: 'string' } }
+      },
+      {
+        $id: 'adr',
+        type: 'object',
+        definitions: { street: { type: 'string' } }
+      }
+    ],
+    buildLocalReference (json, baseUri, fragment, i) {
+      t.equal(URI.serialize(baseUri), baseUriCheck.shift())
+      return escape(json.$id)
+    }
+  }
+
+  const baseUriCheck = [
+    opts.externalSchemas[0].$id,
+    `${opts.applicationUri}/${opts.externalSchemas[1].$id}`
+  ]
+
+  const resolver = RefResolver(opts)
+  const out = resolver.resolve(schema)
+  t.equal(out.properties.user.$ref, '#/definitions/http%3A//hello.absolute/user.json/definitions/name')
+  t.equal(out.properties.address.$ref, '#/definitions/adr/definitions/street')
 })
